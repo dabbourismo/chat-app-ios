86709466f906b0fbd8113cdbed2523bfd909f5ff
Mohamed Dabbour <ortiiga18@Samis-MacBook-Air.local>
2020-11-29 01:17:08 +0200

    Created the app

---
+++ chat-app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  chat-app
+//
+//  Created by Mohamed Dabbour on 11/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        label.text = "Compatiable!"
+    }
+
+
+}
+
